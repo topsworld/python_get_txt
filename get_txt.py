46b944a689e6a3fe0ea4d8ccee16fc811d0c0079
SWorld <13089110907@163.com>
2017-07-11 20:22:10 +0800

    17-7-11

--- get_txt.py
+++ get_txt.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 import re
 import os
 import time
+import sys
 req_head={
 'Cache-Control':'private',
 'CF-RAY':'373fbfa292760d3d-LAX',
@@ -21,7 +22,7 @@ req_header={
 'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
 'Accept-Encoding':'gzip, deflate',
 'Accept-Language':'zh-CN,zh;q=0.8',
-'Cookie':'__cfduid=d11bcead6376e8f217ae5a857516daf5d1499513165; UM_distinctid=15d21f315e1960-0d992e3975d7d8-9383666-1fa400-15d21f315e2b64; bdshare_firstime=1499513165806; tanwanhf_9821=1; CNZZDATA1261736110=726172072-1499509339-%7C1499611939; tanwanhf_9814=1; tanwanhf_9815=1; tanwanhf_9816=1; PPad_id_PP=1; Hm_lvt_5ee23c2731c7127c7ad800272fdd85ba=1498307846,1498391660,1499513166,1499612614; Hm_lpvt_5ee23c2731c7127c7ad800272fdd85ba=1499612658; twtext_10294=1; tanwanpf_9817=2; 91turn_9820=1; bookid=72%2C12%2C10893%2C22441; chapterid=2697168%2C9187%2C4406080%2C8267083; chaptername=%25u7B2C%25u516B%25u767E%25u4E00%25u5341%25u4E09%25u7AE0%2520%25u592A%25u7D20%25u6D77%2C%25u7B2C%25u4E00%25u7AE0%2520%25u51F6%25u5C9B%25u9003%25u72AF%2C%25u7B2C1%25u7AE0%2520%25u51CC%25u4ED9%2C%25u7B2C1%25u7AE0%2520%25u5C11%25u5E74%25u58EE%25u5FD7%25u4E0D%25u8A00%25u6101; tanwanpf_9819=4; bcolor=; font=; size=; fontcolor=; width=',
+'Cookie':'__cfduid=d577ccecf4016421b5e2375c5b446d74c1499765327; UM_distinctid=15d30fac6beb80-0bdcc291c89c17-9383666-13c680-15d30fac6bfa28; CNZZDATA1261736110=1277741675-1499763139-null%7C1499763139; tanwanhf_9821=1; Hm_lvt_5ee23c2731c7127c7ad800272fdd85ba=1499612614,1499672399,1499761334,1499765328; Hm_lpvt_5ee23c2731c7127c7ad800272fdd85ba=1499765328; tanwanpf_9817=1; bdshare_firstime=1499765328088',
 'Host':'www.qu.la',
 'Proxy-Connection':'keep-alive',
 'Referer':'http://www.qu.la/book/1265/765108.html',
@@ -32,17 +33,23 @@ req_header={
 req_url_base='http://www.qu.la/book/'
 
 class myThread (threading.Thread):   #继承父类threading.Thread
-    def __init__(self, threadID, counter):
+    def __init__(self, threadID, counter,start_page):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.counter = counter
+        self.start_page=start_page
     def run(self):                   #把要执行的代码写到run函数里面 线程在创建后会直接运行run函数
         #print("编号为1的小说")
-        get_txt(self.counter)
+        get_txt(self.counter,self.start_page)
         #print("Exiting")
 
-def write_txt_intro(txt):
-    fo = open("txt_intro.txt", "ab+")
+
+threadLock = threading.Lock()
+threads = []
+
+
+def write_txt_intro(txt,start_page):
+    fo = open("txt_intro{0:0>8}_{1:0>8}.txt".format(start_page,start_page+100), "ab+")
     try:
         fo.write(("*#*#{0:0>8}".format(txt['id'])+"\r\n").encode('UTF-8'))
         fo.write(('书籍名称：'+txt['title'] + "\r\n").encode('UTF-8'))
@@ -66,7 +73,7 @@ def write_txt_intro(txt):
 # section_name：章节名称
 # section_text：章节正文
 # section_ct：章节页数
-def get_txt(txt_id):
+def get_txt(txt_id,start_page):
     txt={}
     txt['title']=''
     txt['id']=str(txt_id)
@@ -91,7 +98,7 @@ def get_txt(txt_id):
         print("小说章节页数："+str(section_ct))
         #print("第一章地址寻找成功："+ first_page)
         txt_section=first_page                                                                  #设置现在下载小说章节页面
-        write_txt_intro(txt)
+        write_txt_intro(txt,start_page)
         fo = open('{0:0>8}'.format(txt['id'])+'-'+txt['title'] + '.txt.download', "ab+")         #打开小说文件
         fo.write((txt['title']+"\r\n").encode('UTF-8'))
         fo.write((txt['author'] + "\r\n").encode('UTF-8'))
@@ -113,7 +120,7 @@ def get_txt(txt_id):
                 if(txt_section=='./'):                                                          #判断是否最后一章  最后一章则跳出循环
                     print("编号："+'{0:0>8}   '.format(txt['id'])+  "小说名：《"+txt['title']+"》 下载完成")
                     break
-                fo.write((section_name.text+'\r\n').encode('UTF-8'))                                #以二进制写入章节题目
+                fo.write(('\r'+section_name.text+'\r\n').encode('UTF-8'))                                #以二进制写入章节题目
                 fo.write((section_text).encode('UTF-8'))                        #以二进制写入章节内容
                 #print(txt['title']+' 章节：'+section_name.text+'     已下载')
                 #print(section_text.text.encode('UTF-8'))
@@ -134,15 +141,55 @@ def get_txt(txt_id):
         finally:
             fo_err.close()
 
+#批量获取txt  900-1000
+def get_txts(start_page):
+    print("当前起始页面："+str(start_page))
+    print("正在创建下载任务。")
+    for i in range(start_page, start_page+100):
+        thread_one = myThread(i, str(i),start_page)
+        thread_one.start()
+        threads.append(thread_one)
+    print("下载任务创建完成。")
+    print("等待下载任务完成。。。")
+    task_ct = len(threads)
+    print('********************')
+    i_ls_ct = 0
+    while (1):
+        run_task = 0
+        for i in threads:
+            if (i.isAlive()):
+                run_task += 1
+        # os.system('cls')
+        # write('\b'+"总任务数：" + str(task_ct) + "  已完成任务数：" + str(task_ct - run_task)+"\r")
+        if (i_ls_ct % 10 >= 4):
+            print('{0:0>8}-{1:0>8} '.format(start_page,start_page+100)+"下载中："
+                  + "*" * (int)((task_ct - run_task) / task_ct * 50) + "_" * (int)(
+                run_task / task_ct * 50) + " /. 总数：" + str(task_ct) + "  已完成：" + str(task_ct - run_task), end="\r")
+        else:
+            print('{0:0>8}-{1:0>8} '.format(start_page,start_page+100)+"下载中："
+                  + "*" * (int)((task_ct - run_task) / task_ct * 50) + "_" * (int)(
+                run_task / task_ct * 50) + " \. 总数：" + str(task_ct) + "  已完成：" + str(
+                task_ct - run_task), end="\r")
+        if (run_task == 0):
+            break
+        time.sleep(.1)
+        if (i_ls_ct > 100000):
+            i_ls_ct = 0
+        else:
+            i_ls_ct += 1
+            # print(i_ls_ct)
+    print("所有下载任务已完成")
+    time.sleep(2)
+    os.system('cls' if os.name == 'nt' else "printf '\033c'")
+
 #get_txt(764066)
 if __name__=='__main__':
     #print("请输入需要下载的小说编号：")
     #txt_id = input()
     #get_txt(30176)
     # 创建新线程
-    print("正在创建下载任务。")
-    for i in range(10,13):
-        thread_one= myThread(i, str(i))
-        thread_one.start()
-    print("下载任务创建完成。")
-    print("等待下载任务完成。。。")
+    for i_ls in range(9,50):
+        get_txts(i_ls*100)
+
+#cd get_txt
+#python get_txt.py

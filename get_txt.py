16a18fb9242591225c88b41cdad7617821e6425f
SWorld <13089110907@163.com>
2017-10-16 23:51:44 +0800

    更新 get_txt.py

--- get_txt.py
+++ get_txt.py
@@ -25,7 +25,7 @@ req_header={
 'Cookie':'__cfduid=d577ccecf4016421b5e2375c5b446d74c1499765327; UM_distinctid=15d30fac6beb80-0bdcc291c89c17-9383666-13c680-15d30fac6bfa28; CNZZDATA1261736110=1277741675-1499763139-null%7C1499763139; tanwanhf_9821=1; Hm_lvt_5ee23c2731c7127c7ad800272fdd85ba=1499612614,1499672399,1499761334,1499765328; Hm_lpvt_5ee23c2731c7127c7ad800272fdd85ba=1499765328; tanwanpf_9817=1; bdshare_firstime=1499765328088',
 'Host':'www.qu.la',
 'Proxy-Connection':'keep-alive',
-'Referer':'http://www.qu.la/book/1265/765108.html',
+'Referer':'http://www.qu.la/book/',
 'Upgrade-Insecure-Requests':'1',
 'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36'
 }
